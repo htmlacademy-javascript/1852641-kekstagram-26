e2740884521143091e48f8458100c5d152e2e43c
Roman <rom.13.rs@gmail.com>
2022-06-07 20:34:55 +0300

    Исправил ошибки в функциях

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 const getRandomNumber = (min, max) => {
   if (min > max || min === max || min < 0 || max < 0) {
-    console.log('Ой... Что-то пошло не так!');
+    return 'Ой... Что-то пошло не так!';
   } else {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-}
+};
+const maxLength = (string, length) => string.length === length;
 
-function maxLength (string, length) {
-  return string.length === length;
-}
-
-
+getRandomNumber(2, 5);
+maxLength('hello', 5);
